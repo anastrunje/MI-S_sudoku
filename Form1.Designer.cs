3673b06bad8525c9402b9ed4f78af9f0ebe3fa23
anastrunje <astrunje@pmfst.hr>
2022-05-17 14:32:56 +0200

    usporko aplikacija bez speech recognition

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.AdvancedLevel = new System.Windows.Forms.RadioButton();
+            this.IntermediateLevel = new System.Windows.Forms.RadioButton();
+            this.beginnerLevel = new System.Windows.Forms.RadioButton();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(967, 611);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Level";
+            // 
+            // AdvancedLevel
+            // 
+            this.AdvancedLevel.AutoSize = true;
+            this.AdvancedLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.AdvancedLevel.Location = new System.Drawing.Point(973, 708);
+            this.AdvancedLevel.Margin = new System.Windows.Forms.Padding(2);
+            this.AdvancedLevel.Name = "AdvancedLevel";
+            this.AdvancedLevel.Size = new System.Drawing.Size(114, 28);
+            this.AdvancedLevel.TabIndex = 9;
+            this.AdvancedLevel.Text = "Advanced";
+            this.AdvancedLevel.UseVisualStyleBackColor = true;
+            // 
+            // IntermediateLevel
+            // 
+            this.IntermediateLevel.AutoSize = true;
+            this.IntermediateLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.IntermediateLevel.Location = new System.Drawing.Point(973, 676);
+            this.IntermediateLevel.Margin = new System.Windows.Forms.Padding(2);
+            this.IntermediateLevel.Name = "IntermediateLevel";
+            this.IntermediateLevel.Size = new System.Drawing.Size(131, 28);
+            this.IntermediateLevel.TabIndex = 10;
+            this.IntermediateLevel.Text = "Intermediate";
+            this.IntermediateLevel.UseVisualStyleBackColor = true;
+            // 
+            // beginnerLevel
+            // 
+            this.beginnerLevel.AutoSize = true;
+            this.beginnerLevel.Checked = true;
+            this.beginnerLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.beginnerLevel.Location = new System.Drawing.Point(973, 644);
+            this.beginnerLevel.Margin = new System.Windows.Forms.Padding(2);
+            this.beginnerLevel.Name = "beginnerLevel";
+            this.beginnerLevel.Size = new System.Drawing.Size(105, 28);
+            this.beginnerLevel.TabIndex = 11;
+            this.beginnerLevel.TabStop = true;
+            this.beginnerLevel.Text = "Beginner";
+            this.beginnerLevel.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(973, 117);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(119, 81);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear Input";
+            this.clearButton.UseVisualStyleBackColor = true;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.Location = new System.Drawing.Point(973, 32);
+            this.checkButton.Margin = new System.Windows.Forms.Padding(2);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(119, 81);
+            this.checkButton.TabIndex = 8;
+            this.checkButton.Text = "Check Input";
+            this.checkButton.UseVisualStyleBackColor = true;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newGameButton.Location = new System.Drawing.Point(973, 757);
+            this.newGameButton.Margin = new System.Windows.Forms.Padding(2);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(119, 77);
+            this.newGameButton.TabIndex = 6;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(38, 32);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 900);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 914);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AdvancedLevel);
+            this.Controls.Add(this.IntermediateLevel);
+            this.Controls.Add(this.beginnerLevel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton AdvancedLevel;
+        private System.Windows.Forms.RadioButton IntermediateLevel;
+        private System.Windows.Forms.RadioButton beginnerLevel;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
